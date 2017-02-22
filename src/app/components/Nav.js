64bf9fdad4b5867986c481e2d2e5454b48a7561e
arraqib <raqibjs93@gmail.com>
2017-02-22 15:05:28 +0600

    Init React Foundation app

--- src/app/components/Nav.js
+++ src/app/components/Nav.js
@@ -2,17 +2,39 @@ import React from 'react';
 import { Link, IndexLink } from 'react-router';
 
 
+export default class Nav extends React.Component {
 
-const Nav = () => {
-    return (
-        <div>
-            <IndexLink to="/" activeClassName="active" activeStyle={{ fontWeight: 'bold' }}>
-                Get Weather
-                    </IndexLink>
-            <Link to="/about" activeClassName="active" activeStyle={{ fontWeight: 'bold' }}>About </Link>
-            <Link to="/examples" activeClassName="active" activeStyle={{ fontWeight: 'bold' }}>Examples</Link>
-        </div>
-    )
+    onSearch(e){
+        e.preventDefault();
+        alert('Wire it up');
+    }
+
+    render() {
+        return (
+            <div className='top-bar'>
+                <div className='top-bar-left'>
+                    <ul className="menu">
+                        <li className="menu-text">Get Weather</li>
+                        <li><IndexLink to="/" activeClassName="active" activeStyle={{ fontWeight: 'bold' }}>
+                            Home
+                    </IndexLink></li>
+                        <li><Link to="/about" activeClassName="active" activeStyle={{ fontWeight: 'bold' }}>About </Link></li>
+                        <li><Link to="/examples" activeClassName="active" activeStyle={{ fontWeight: 'bold' }}>Examples</Link></li>
+                    </ul>
+                </div>
+                <div className='top-bar-right'>
+                    <form onSubmit={this.onSearch}>
+                        <ul className='menu'>
+                            <li>
+                                <input type='search' placeholder='Search Weather' />
+                            </li>
+                            <li>
+                                <input type='submit' className='button' value='Get Weather' />
+                            </li>
+                        </ul>
+                    </form>
+                </div>
+            </div>
+        )
+    }
 }
-
-export default Nav;
